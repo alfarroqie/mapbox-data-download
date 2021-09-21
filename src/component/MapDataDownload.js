6f00f2b9dbc083ff434da7834a79f54671f4a224
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2021-09-21 15:33:30 +0700

    make hover and popup

--- src/component/MapDataDownload.js
+++ src/component/MapDataDownload.js
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-
 import kabupatenGeojson from '../data/kabupaten.geojson'
 import dataDownload from '../data/data-download.json'
 
@@ -38,7 +37,17 @@ export default function MapDataDownload() {
       map.current.addSource('kabupaten', {
         'type': 'geojson',
         'data': kabupatenGeojson,
-        'promoteId': 'ID_KAB'
+        'promoteId': 'KABUPATEN'
+      });
+      //mapping data download to feature state
+      dataDownload.forEach((data) => {
+        map.current.setFeatureState({
+          source: 'kabupaten',
+          id: data.location,
+        },
+        {
+          AVG_DOWNLOAD: data.avg_download_throughput
+        })
       });
       //fill cities layer
       map.current.addLayer({
@@ -47,12 +56,23 @@ export default function MapDataDownload() {
         'source': 'kabupaten', // reference the data source
         'layout': {},
         'paint': {
-          'fill-color': 'red', // red color fill
+          'fill-color': [
+            'case',
+            ['>', ['feature-state', 'AVG_DOWNLOAD'], 15000],
+            '#B71C1C',
+            ['>=', ['feature-state', 'AVG_DOWNLOAD'], 10000],
+            '#EF5350',
+            ['>=', ['feature-state', 'AVG_DOWNLOAD'], 5000],
+            '#FFCDD2',
+            ['>=', ['feature-state', 'AVG_DOWNLOAD'], 0],
+            '#FFEBEE',
+            '#FFFFFF'
+          ],
           'fill-opacity': [
             'case',
             ['boolean', ['feature-state', 'hover'], false],
-            0.6,
-            0.3
+            1,
+            0.7
             ]
         }
       });
@@ -80,38 +100,79 @@ export default function MapDataDownload() {
           'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold']
           }
         });
-
-      //hover
-      var hoverKabId = null;
-      map.current.on('mousemove', 'fill-cities', (e) => {
-        if (e.features.length > 0) {
-          if (hoverKabId !== null) {
-            map.current.setFeatureState(
-            { source: 'kabupaten', id: hoverKabId },
-            { hover: false }
-            );
-          }
-          hoverKabId = e.features[0].id;
-          map.current.setFeatureState(
-          { source: 'kabupaten', id: hoverKabId },
-          { hover: true }
-          );
-        }
-      });
-        
-      // When the mouse leaves the state-fill layer, update the feature state of the
-      // previously hovered feature.
-      map.current.on('mouseleave', 'fill-cities', () => {
-        if (hoverKabId !== null) {
-          map.current.setFeatureState(
-          { source: 'kabupaten', id: hoverKabId },
-          { hover: false }
-          );
-        }
-        hoverKabId = null;
-      });
     });
   });
+  //HOVER
+  useEffect(() => {
+    if(!map.current) return;
+     var hoverKabId = null;
+     map.current.on('mousemove', 'fill-cities', (e) => {
+       if (e.features.length > 0) {
+         if (hoverKabId !== null) {
+           map.current.setFeatureState(
+           { source: 'kabupaten', id: hoverKabId },
+           { hover: false }
+           );
+         }
+         hoverKabId = e.features[0].id;
+         map.current.setFeatureState(
+         { source: 'kabupaten', id: hoverKabId },
+         { hover: true }
+         );
+       }
+     });
+     // When the mouse leaves the state-fill layer, update the feature state of the
+     // previously hovered feature.
+     map.current.on('mouseleave', 'fill-cities', () => {
+       if (hoverKabId !== null) {
+         map.current.setFeatureState(
+         { source: 'kabupaten', id: hoverKabId },
+         { hover: false }
+         );
+       }
+       hoverKabId = null;
+     });
+  })
+  //POPUP
+  useEffect(() => {
+    const popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
+    });
+    //hover for popup
+    map.current.on('mousemove', 'fill-cities', (e) => {
+      // Change the cursor style as a UI indicator.
+      map.current.getCanvas().style.cursor = 'pointer';
+      
+      // Copy coordinates array.
+      const { lat, lng } = e.lngLat;
+      // const coordinates = e.features[0].geometry.coordinates.slice();
+      const region = e.features[0].properties.REGION;
+      const kabupaten = e.features[0].properties.KABUPATEN;
+      const avgDownload = e.features[0].state.AVG_DOWNLOAD;
+      // // Ensure that if the map is zoomed out such that multiple
+      // // copies of the feature are visible, the popup appears
+      // // over the copy being pointed to.
+      // while (Math.abs(e.lngLat.lng - coordinates) > 180) {
+      //   coordinates += e.lngLat.lng > coordinates ? 360 : -360;
+      // }
+      
+      // Populate the popup and set its coordinates
+      // based on the feature found.
+      popup.setLngLat([lng, lat]).setHTML(
+        `
+          <h1>REGION: <b>${region} </b></h1>
+          <h1> KABUPATEN: <b>${kabupaten}</b></h1>
+          <h1> AVERAGE DOWNLOAD: <b>${avgDownload}</b></h1>
+        `
+        ).addTo(map.current);
+    });
+    map.current.on('mouseleave', 'fill-cities', () => {
+      map.current.getCanvas().style.cursor = '';
+      popup.remove();
+    });
+  })
+  
 
   return (
   <div>
